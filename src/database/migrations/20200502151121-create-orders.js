4b0a4fa8ba7c1b67c0f5a757bb551b8c2cd3d815
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-05-02 12:24:27 -0300

    Orders migration

---
+++ src/database/migrations/20200502151121-create-orders.js
@@ -0,0 +1,70 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      recipient_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'recipients',
+          key: 'id',
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL',
+          allowNull: false,
+        },
+      },
+      courier_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'couriers',
+          key: 'id',
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL',
+          allowNull: false,
+        },
+      },
+      signature_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'files',
+          key: 'id',
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL',
+          allowNull: false,
+        },
+      },
+      product: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('orders');
+  },
+};
