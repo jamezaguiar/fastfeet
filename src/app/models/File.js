7adca222b390486926ac1aee9c0ffe3fe7cf564a
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-05-02 12:05:32 -0300

    Listagem de entregadores com url para foto

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import Sequelize, { Model } from 'sequelize';
 
 class File extends Model {
@@ -6,6 +7,12 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `${process.env.APP_URL}/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
